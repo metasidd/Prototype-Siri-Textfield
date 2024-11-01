857b87d6fb86aa61b6f57a97bc4b657276860792
Siddhant Mehta <siddhant.olive92@gmail.com>
2024-11-01 15:34:39 +0530

    Empty project

---
+++ Prototype-Siri-Textfield/Prototype_Siri_TextfieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prototype_Siri_TextfieldApp.swift
+//  Prototype-Siri-Textfield
+//
+//  Created by Siddhant Mehta on 2024-11-01.
+//
+
+import SwiftUI
+
+@main
+struct Prototype_Siri_TextfieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
